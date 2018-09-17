0928525f803dfad227eb79aa4d2b55d4399e01a8
Camilo Posada Angel <camilo.posada@sofka.com.co>
2018-09-17 14:54:30 -0500

    Se añade configuración de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiCWJdqptqOZ0OYsOVRNA8AMlrAzvxEOM",
+    authDomain: "sofka-notes.firebaseapp.com",
+    databaseURL: "https://sofka-notes.firebaseio.com",
+    projectId: "sofka-notes",
+    storageBucket: "sofka-notes.appspot.com",
+    messagingSenderId: "730911811629"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDiCWJdqptqOZ0OYsOVRNA8AMlrAzvxEOM",
+    authDomain: "sofka-notes.firebaseapp.com",
+    databaseURL: "https://sofka-notes.firebaseio.com",
+    projectId: "sofka-notes",
+    storageBucket: "sofka-notes.appspot.com",
+    messagingSenderId: "730911811629"
+  }
 };
 
 /*
